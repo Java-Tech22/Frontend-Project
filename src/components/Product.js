402e00a0e07d18e7eedb4b83b5847f3ee9aac05f
Preeti <preeti@binarylogicit.com>
2024-06-22 17:52:11 -0400

    Added functionality with Product.json

--- src/components/Product.js
+++ src/components/Product.js
@@ -15,14 +15,14 @@ export default function Product ( {product}) {
 
     return (
                 <div key= {product.id}  className='product'>
-                    {/* <img src={`images/${product.image}` } alt={product.title}  onError={errorHandler}/>
+                     <img src={`images/${product.image}` } alt={product.title}  onError={errorHandler}/>
                     <div className='product-info'>
                         <h3 className='product-title'>{product.title}</h3>
                         <p className={`product-rating ${getRating(product.rating)}`}>{product.rating}</p>
                         <p className='product-price'>{product.price}</p>
                         <p className='product-description'>{product.description}</p>
-                    </div> */}
+                    </div> 
                 </div>
-        
+    
     );
 }
--- src/components/ProductsGrid.js
+++ src/components/ProductsGrid.js
@@ -68,23 +68,9 @@ return (
 
     <div className='products-grid'>
         {
-            //filteredProducts.map(product => (
-
-                products.map(product => (
-
-
-
-                    <div key= {product.id}  className='product'>
-                            <img src={`images/${product.image}` } alt={product.title} />
-                            <div className='product-info'>
-                                <h3 className='product-title'>{product.title}</h3>
-                                <p className='product-rating'>{product.rating}</p>
-                                <p className='product-price'>{product.price}</p>
-                                <p className='product-description'>{product.description}</p>
-                            </div>
-                        </div>
+            filteredProducts.map(product => (
             
-                //<Product product={product} key={product.id}></Product>
+                <Product product={product} key={product.id}></Product>
             ))
         }
     </div>
